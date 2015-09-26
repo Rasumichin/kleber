4db62ce3c22b2b177034246217f4ca00f845740c
Erik Lotz <elo@jsmithy.de>
2015-09-26 21:19:54 +0200

    Refacoring, extract method.

--- src/test/java/de/jsmithy/zen/stickr/MetaAlbumTest.java
+++ src/test/java/de/jsmithy/zen/stickr/MetaAlbumTest.java
@@ -1,27 +1,31 @@
 package de.jsmithy.zen.stickr;
 
-import org.junit.Test;
-
+import org.junit.*;
 import static org.junit.Assert.*;
 
 public class MetaAlbumTest {
 
-	@Test
-	public void testCreate() {
-		String title = "Benjamin Bl�mchen - Mein gro�es Stickeralbum";
-		Topic topic = Topic.create(title);
-		Publisher publisher = Publisher.create("Panini");
+    @Test
+    public void testCreate() {
+        Topic topic = getTopic();
+        Publisher publisher = Publisher.create("Panini");
 
-		MetaAlbum sut = MetaAlbum.create(topic, publisher);
-		assertNotNull("Erstellung war nicht korrekt.", sut);
+        MetaAlbum sut = MetaAlbum.create(topic, publisher);
+        assertNotNull("Erstellung war nicht korrekt.", sut);
 
-		boolean isEqual = topic.equals(sut.getTopic());
-		assertTrue("Topic war nicht gleich.", isEqual);
+        boolean isEqual = topic.equals(sut.getTopic());
+        assertTrue("Topic war nicht gleich.", isEqual);
 
-		isEqual = publisher.equals(sut.getPublisher());
-		assertTrue("Publisher war nicht gleich.", isEqual);
+        isEqual = publisher.equals(sut.getPublisher());
+        assertTrue("Publisher war nicht gleich.", isEqual);
 
-		int numberOfPlaceholders = sut.countPlaceholders();
-		assertEquals("Placeholder sind nicht leer.", 0, numberOfPlaceholders);
-	}
+        int numberOfPlaceholders = sut.countPlaceholders();
+        assertEquals("Placeholder sind nicht leer.", 0, numberOfPlaceholders);
+    }
+    
+    private Topic getTopic(){    
+        String title = "Benjamin Bl�mchen - Mein gro�es Stickeralbum";
+        Topic topic = Topic.create(title);
+        return topic;
+    }  
 }
